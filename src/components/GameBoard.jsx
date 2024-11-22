a1b740438be23d79c237660f897bbd9462e0c3bb
Kacper Mirocha <74498395+Ereffe@users.noreply.github.com>
2024-11-22 12:32:16 +0100

    Add GameBoard component and integrate it into App; implement game board state management

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,14 +1,36 @@
-const initalGameBoard = [
+import { useState } from "react";
+const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
-export default function Gameboard() {
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]), //deep copy
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
   return (
     <ol id="game-board">
-      {initalGameBoard.map((row, rowIndex) => {
-        <li key={rowIndex}></li>;
-      })}
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
     </ol>
   );
 }
